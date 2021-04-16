a3d503a16de0956dcc915d8584fff5ca421c09a7
lena <alena.litouka@gmail.com>
2021-04-16 00:36:37 -0400

    half of the lab

--- src/main/java/JerksonParser.java
+++ src/main/java/JerksonParser.java
@@ -1,25 +1,65 @@
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.deser.DataFormatReaders;
+import com.fasterxml.jackson.databind.util.JSONPObject;
+
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 public class JerksonParser {
    private String outputData;
+   private ArrayList<String> output = new ArrayList<String>();
 
-   public JerksonParser() throws Exception { this.outputData = (new Main()).readRawDataToString(); }
-
-   public String getOutputData() { return outputData;}
-
-   public String changeNaMeToName(){
-      Pattern pattern = Pattern.compile("Name",Pattern.CASE_INSENSITIVE);
-      Matcher matcher1 = pattern.matcher(outputData);
-      boolean matchFound = matcher1.find();
-      String newOutput = outputData;
-
-      if(matchFound){
-         newOutput = matcher1.replaceAll("name");
-      }
-
-      return newOutput;
+   public JerksonParser(String outputData) {
+      this.outputData = outputData;
    }
-}
+
+   public String getOutputData() {
+      return outputData;
+   }
+
+   public String[] getIndividualLines(String input){
+     Pattern pattern = Pattern.compile("##");
+     String[] result = pattern.split(input);
+     return result;
+   }
+
+   public String  convertStringToJsonString (String productLine){
+       Pattern patternSemicolon = Pattern.compile("[;^%@!*]");
+       Matcher matcherSemicolon = patternSemicolon.matcher(productLine);
+       String jsonString=productLine;
+       jsonString= matcherSemicolon.replaceAll("\", \"");
+
+       Pattern patternColon = Pattern.compile(":");
+       Matcher matcherColon = patternColon.matcher(jsonString);
+       jsonString= matcherColon.replaceAll("\": \"");
+       return "{\"" + jsonString + "\"}";
+
+   }
+
+
+
+   public Product jsonStringToObject (String productLine) throws JsonProcessingException {
+       ObjectMapper objectMapper = new ObjectMapper();
+       Product newProduct = objectMapper.readValue(productLine, Product.class);
+
+       return newProduct;
+   }
+
+   }
+//   public  String changeHamletToLeon(){
+//      Pattern pattern =Pattern.compile("[hH][aA][mM][lL][eE][tT]");
+//      Matcher matcher=pattern.matcher(hamletData);
+//      boolean matchFound = matcher.find();
+//      String newPoem=hamletData;
+//
+//      if(matchFound){
+//         newPoem = matcher.replaceAll("Leon");
+//      }
+//
+//      return newPoem;
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,11 @@
 import org.apache.commons.io.IOUtils;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Main {
 
@@ -9,10 +15,16 @@ public class Main {
         return result;
     }
 
-    public static void main(String[] args) throws Exception{
-       // String output = (new Main()).readRawDataToString();
-        //System.out.println(output);
-    JerksonParser jerksonParser = new JerksonParser();
-        System.out.println(jerksonParser.changeNaMeToName());
+    public static void main(String[] args) throws Exception, NoClassDefFoundError{
+        String input = (new Main()).readRawDataToString().toLowerCase();
+        System.out.println(input);
+        JerksonParser jerksonParser = new JerksonParser(input);
+      String[] output= jerksonParser.getIndividualLines(input);
+       for(String s:output){
+          String jsonString=jerksonParser.convertStringToJsonString(s); //"{ \"name\" : \"Black\", \"type\" : \"BMW\",\"price\" : \"Black\", \"expiration\" : \"BMW\" }"; //
+           Product p=jerksonParser.jsonStringToObject(jsonString);
+           System.out.println(p.getName());
+
+       }
     }
 }
